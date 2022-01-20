9be2f23da28f4d577b67bbf8b6af47d99b227783
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2022-01-20 11:32:55 +0100

    Model > models update

--- lib/models/Party.dart
+++ lib/models/Party.dart
@@ -1,6 +1,6 @@
 import 'User.dart';
 
-enum Theme {
+enum ThemeParty {
   happyHour,
   dinner,
 }
@@ -16,21 +16,49 @@ class AttendeeParty {
 }
 
 class Party {
-  String id;
+  String? id;
   User user;
-  Theme theme;
-  String picturePath;
-  List<AttendeeParty> attendeesParty;
+  String theme;
+  String? picturePath;
+  List<AttendeeParty>? attendeesParty;
   bool isValid;
   DateTime createdAt;
+  DateTime partyDateTime;
 
-  Party(
+  Party({
     this.id,
     this.picturePath,
-    this.attendeesParty, {
+    this.attendeesParty,
     required this.user,
     required this.theme,
     this.isValid = false,
     required this.createdAt,
+    required this.partyDateTime,
   });
+
+  factory Party.fromJson(Map<String, dynamic> json) {
+    return Party(
+      id: json['id'] != null ? json['id']! as String : "",
+      picturePath: json['picturePath'] != null ? json['picturePath']! as String : "",
+      attendeesParty: json['attendeesParty'] != null ? json['attendeesParty']! as List<AttendeeParty> : [],
+      user: json['user'] as User,
+      theme: json['theme'] as String,
+      isValid: json['isValid']  as bool,
+      createdAt: DateTime.parse(json['createdAt']! as String),
+      partyDateTime: DateTime.parse(json['partyDateTime']! as String),
+    );
+  }
+
+  Map<String, Object?> toJson() {
+    return {
+      'id': id,
+      'picturePath': picturePath,
+      'attendeesParty': attendeesParty,
+      'user': user,
+      'theme': theme,
+      'isValid': isValid,
+      'createdAt': createdAt,
+      'partyDateTime': partyDateTime,
+    };
+  }
 }
